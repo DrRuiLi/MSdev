b10b7125400bae73731a6835913442efebb663ba
liRui <lirui3@shanghaitech.edu.cn>
2022-07-25 20:59:52 +0800

    add function metabolomic_work_flow

---
+++ R/metabolomic_work_flow.R
@@ -0,0 +1,58 @@
+#' Title
+#'
+#' @param project.dir
+#' @param raw.data.dir
+#'
+#' @return
+#' @export
+#'
+#' @examples
+metabolomic_work_flow <- function(
+    project.dir ="d:/2022_07_05-Lirui/",
+    raw.data.dir ="d:/2022_07_05-Lirui/Data/"
+    ){
+
+
+  thread <- parallel::detectCores()-1
+
+
+
+
+  ### MS file convert and information
+  {
+
+    ms.ana <- export_sample_information_from_wiff(raw.data.dir = raw.data.dir,
+                                                  project.dir = project.dir)
+
+    #ms.ana$sample.info <- readxl::read_excel(paste0(project.dir,"/sample.info.xlsx"))
+
+    ms.ana <- ms_convert(ms.ana)
+
+  }
+
+  ### xcms processing
+  {
+
+    ms.ana <- xcms_dda_processing(ms.ana ,  polarity = "positive")
+    ms.ana <- xcms_dda_processing(ms.ana ,  polarity = "negative")
+    ms.ana <- extract_spectra(ms.ana)
+
+  }
+  ### annotation
+  {
+
+    ms.ana <- annotation_by_database(ms.ana,polarity = "positive")
+    ms.ana <- annotation_by_database(ms.ana,polarity = "negative")
+
+  }
+
+
+
+
+
+
+
+
+
+
+}
