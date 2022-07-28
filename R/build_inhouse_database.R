45cbdf932cacc319f08a4ad76a902d48889d2e9a
liRui <lirui3@shanghaitech.edu.cn>
2022-07-28 17:08:04 +0800

    add function

---
+++ R/build_inhouse_database.R
@@ -0,0 +1,84 @@
+#' @title build inhouse database
+#'
+#' @param record.file
+#' @param mzML.files
+#'
+#' @return
+#' @export
+#' @import xcms
+#' @examples
+build_inhouse_database <-
+  function(compound.record.file ) {
+
+    ### test
+    compound.record.file<- "C:\\Users\\91879\\OneDrive\\Documents\\Code\\R\\Projecct\\2022.7.22.MS.inhouse.database.building\\Standard.record.2021.12.18.STD_01.xlsx"
+    ###check files
+    {
+      if (!file.exists(compound.record.file)) {
+        stop.info <- paste0(compound.record.file , " not exist ") %>%
+          crayon::red()
+        stop(stop.info)
+      }
+
+
+      compound.record <- Retrieve_compounds_data_from_pubchem(compound.record.file)
+       if (is.null(compound.record$mzML.positive)) {
+         message("Select positive mzML file")
+         compound.record$mzML.positive <-
+           choose.files(default = getwd(), caption = "Select positive")
+         openxlsx::write.xlsx(compound.record , file = compound.record.file)
+       }
+       if (is.null(compound.record$mzML.negative)) {
+         message("Select negative mzML file")
+         compound.record$mzML.negative <-
+           choose.files(default = getwd(), caption = "Select negative")
+         openxlsx::write.xlsx(compound.record , file = compound.record.file)
+       }
+    }
+
+
+  ###pos
+    {
+
+      xcms.pos <- readMSData(unique(compound.record$mzML.positive),
+                             mode = "onDisk")
+      centwave.param <- CentWaveParam(peakwidth = c(5,30),
+                                      prefilter = c(3,100),
+                                      snthresh = 100,
+                                      ppm = 10)
+      xcms.pos<-findChromPeaks(xcms.pos,
+                                param = centwave.param,
+                               BPPARAM = SerialParam())
+      plot_xcms_peaks_distribution(xcms.pos)
+      xcms.pos.peaks <-chromPeaks(xcms.pos)
+      MS.network.pos <- expand_adduct_from_compounds(compound.record,"positive")
+      MS.network.pos <- match_adduct_to_peaks(MS.network.pos,xcms.pos.peaks,ppm.thresh = 10)
+      #plot_adduct_distribution(MS.network.pos,5)
+
+
+    }
+    ###neg
+    {
+
+      xcms.neg <- readMSData(unique(compound.record$mzML.negative),
+                             mode = "onDisk")
+      centwave.param <- CentWaveParam(peakwidth = c(5,30),
+                                      prefilter = c(3,100),
+                                      snthresh = 100,
+                                      ppm = 10)
+      xcms.neg<-findChromPeaks(xcms.neg,
+                               param = centwave.param,
+                               BPPARAM = SerialParam())
+      plot_xcms_peaks_distribution(xcms.neg)
+      xcms.neg.peaks <-chromPeaks(xcms.neg)
+      MS.network.neg <- expand_adduct_from_compounds(compound.record,"negative")
+      MS.network.neg <- match_adduct_to_peaks(MS.network.neg,xcms.neg.peaks,ppm.thresh = 10)
+      #plot_adduct_distribution(MS.network.pos,3)
+
+
+    }
+
+
+
+
+  }
---
+++ R/plot_adduct_distribution.R
@@ -0,0 +1,23 @@
+plot_adduct_distribution <- function(MS.network,index){
+
+###test
+  compound <- MS.network[[index]][["compound"]]
+  adduct.table <-MS.network[[index]][["adduct"]]
+###
+  ggplot(adduct.table)+
+    geom_point(aes(x = peak.rt,y=peak.mz,size = log10(peak.intb),col = adduct),
+               alpha = 0.8)+
+    ggsci::scale_color_npg()+
+    scale_size(range = c(3,5),breaks = c(2,4.5,5))+
+    xlim(c(0,750))+
+    labs(title = compound["name"],
+         subtitle = paste0("exact mass = ",compound["exact.mass"]),
+         x = "Retention time",
+         y = "mz",
+         col = "Adduct form",
+         size = "Log10(intensity)")+
+    theme(text = element_text(size = 8))->adduct.plot
+  return(adduct.plot)
+  #export::graph2png(file = "b.png",width = 5,height = 4)
+
+}
