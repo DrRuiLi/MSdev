4d07d6ce2276f6bd9dd302fcf5f86b5e1c66f1a9
LiRui <918791461@qq.com>
2022-08-03 16:58:20 +0800

    debug

--- R/build_inhouse_database_single_injection.R
+++ R/build_inhouse_database_single_injection.R
@@ -13,7 +13,7 @@ build_inhouse_database_single_injection <-
     ### test
     compound.record.file<- "C:\\Users\\91879\\OneDrive\\Documents\\Code\\R\\Projecct\\2022.7.22.MS.inhouse.database.building\\Standard.record.2021.12.18.STD_01.xlsx"
     compound.record.file<- "d:/2022.6.27.STD/Internal.standard.2022.6.27.xlsx"
-
+    compound.record.file<- "C:\\Users\\91879\\OneDrive\\Documents\\Code\\R\\Default_Workspace\\Internal.standard.2022.3.7.xlsx"
     ###check files
     {
       if (!file.exists(compound.record.file)) {
@@ -44,7 +44,7 @@ build_inhouse_database_single_injection <-
 
       xcms.pos <- readMSData(unique(compound.record$mzML.positive),
                              mode = "onDisk")
-      centwave.param <- CentWaveParam(peakwidth = c(5,30),
+      centwave.param <- CentWaveParam(peakwidth = c(5,50),
                                       prefilter = c(3,100),
                                       snthresh = 100,
                                       ppm = 10)
@@ -55,7 +55,7 @@ build_inhouse_database_single_injection <-
       xcms.pos.peaks <-chromPeaks(xcms.pos)
       MS.network.pos <- expand_adduct_from_compounds(compound.record,"positive")
       MS.network.pos <- match_adduct_to_peaks(MS.network.pos,xcms.pos.peaks,ppm.thresh = 10)
-      plot_adduct_distribution(MS.network.pos,5)
+      plot_adduct_distribution(MS.network.pos,3)
 
 
     }
@@ -75,7 +75,7 @@ build_inhouse_database_single_injection <-
       xcms.neg.peaks <-chromPeaks(xcms.neg)
       MS.network.neg <- expand_adduct_from_compounds(compound.record,"negative")
       MS.network.neg <- match_adduct_to_peaks(MS.network.neg,xcms.neg.peaks,ppm.thresh = 10)
-      plot_adduct_distribution(MS.network.neg,5)
+      plot_adduct_distribution(MS.network.neg,3)
 
 
     }
