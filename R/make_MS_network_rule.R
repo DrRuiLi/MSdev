fe68d1c038f1670a26fc3718180574b6801444d1
LiRui <918791461@qq.com>
2022-08-05 16:29:04 +0800

    update formate adducts

--- R/make_MS_network_rule.R
+++ R/make_MS_network_rule.R
@@ -3,6 +3,16 @@
 make_MS_network_rule <- function() {
   data("isotopes", package = "enviPat")
   data("adducts", package = "enviPat")
+  data("empirical_rules_from_NetID")
+
+  adduct_new <- adducts %>%
+    rowwise()%>%
+    mutate(Formula_add = case_when(Formula_add == "FALSE"~ "C0",
+                                   T ~ Formula_add),
+           Formula_ded = case_when(Formula_ded == "FALSE"~ "C0",
+                                   T ~ Formula_ded),
+           Formula_diff = formula_calculate_lc8(Formula_add,Formula_ded, -1))%>%
+    select(-Formula_add,-Formula_ded)
 
 
 
