90e9df47cb68fff3509c955d441360eb8c5a9f46
LiRui <918791461@qq.com>
2022-08-05 21:11:45 +0800

    update

--- R/make_MS_network_rule.R
+++ R/make_MS_network_rule.R
@@ -5,7 +5,7 @@ make_MS_network_rule <- function() {
   data("adducts", package = "enviPat")
   data("empirical_rules_from_NetID")
 
-  adduct_new <- adducts %>%
+  adduct_MSdev <- adducts %>%
     rowwise()%>%
     mutate(Formula_add = case_when(Formula_add == "FALSE"~ "C0",
                                    T ~ Formula_add),
