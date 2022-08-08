902c0efff48d99867481c32be1295d58897d9b46
liRui <lirui3@shanghaitech.edu.cn>
2022-08-08 15:16:34 +0800

    update add isotope pattern before adduct

--- R/expand_adduct_from_compounds.R
+++ R/expand_adduct_from_compounds.R
@@ -1,46 +1,61 @@
 #' @title  Expand compound to adduct
-#' @description Using formula, exact.mass and polarity, expand adduct by enviPat::adduct
+#' @description Using formula, exact.mass and ion_mode, expand adduct by enviPat::adduct
 #'
 #' @param compound.record
-#' @param polarity
+#' @param ion_mode
 #'
 #' @return
 #' @export
 #'
 #' @examples
-expand_adduct_from_compounds <- function(compound.record, polarity) {
-  adduct.rule <- adducts %>%
-    dplyr::filter(Ion_mode == polarity) %>%
-    dplyr::mutate(
-      Formula_add = case_when(Formula_add == "FALSE" ~  "C0" , T ~ Formula_add),
-      Formula_ded = case_when(Formula_ded == "FALSE" ~  "C0" , T ~ Formula_ded)
-    )
+expand_adduct_from_compounds <- function(compound.record, ion_mode) {
+  data("adduct_MSdev")
+  adduct.rule <- adduct_MSdev %>%
+    dplyr::filter(Ion_mode == ion_mode)
+
   fff <- function(x) {
+    #x <- compound.record[1,]
     to.return <- list()
     to.return[["compound"]] <- x
     x.formula <- x["formula"]
     x.mass <- check_chemform(isotopes,x.formula)$monoisotopic_mass
-    to.return[["adduct.candidate"]] <- adduct.rule %>%
-      rowwise() %>%
-      mutate(
-        formula =  multiform(formula1 = x.formula , fact = Multi),
-        formula = mergeform(formula, Formula_add),
-        formula = subform(formula, Formula_ded),
-        adduct = Name,
-        charge = Charge,
-        multi = Multi,
-        ion_mode = Ion_mode,
-        form = "adduct",
-        exact.mz = x.mass * Multi / abs(Charge) + Mass
-      ) %>%
-      select("formula"  ,
-             "adduct",
-             "charge",
-             "multi",
-             "ion_mode",
-             "form",
-             "exact.mz")
+    x.isopat <- isotopes_pattern_enviPat(x.formula)%>%
+      mutate(form = paste0(isotope_element , "M"))
 
+    x_adduct <-data.frame()
+    for (i in 1:nrow(x.isopat)) {
+
+      m_formula <- x.isopat$formula[i]
+      m_mass <- x.isopat$m.z[i]
+      m_form <- x.isopat$form[i]
+      m_adduct <- adduct.rule %>%
+        rowwise() %>%
+        mutate(
+          formula =  multiform(formula1 = m_formula , fact = Multi),
+          formula = formula_calculate_lc8(formula , Formula_diff),
+          adduct = Name,
+          adduct = sub(pattern = "M",replacement = m_form , x = adduct),
+          charge = Charge,
+          multi = Multi,
+          ion_mode = Ion_mode,
+          form = "adduct",
+          exact.mz = m_mass * Multi / abs(Charge) + Mass
+        ) %>%
+        select("formula"  ,
+               "adduct",
+               "charge",
+               "multi",
+               "ion_mode",
+               "form",
+               "exact.mz")
+      m_adduct
+      x_adduct <- rbind(x_adduct , m_adduct)
+
+    }
+
+
+
+    to.return[["adduct.candidate"]] <- x_adduct
     return(to.return)
 
   }
