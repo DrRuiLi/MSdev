258d410a1764e75cdb2a81f8c549c46170a0edae
liRui <lirui3@shanghaitech.edu.cn>
2022-08-09 14:39:05 +0800

    update variabls name

--- R/match_adduct_to_features.R
+++ R/match_adduct_to_features.R
@@ -19,28 +19,28 @@ match_adduct_to_features <-
      # x <- MS.network[[2]]
       adduct.candidate <- x[["adduct.candidate"]]
       adduct.mz <- adduct.candidate$exact.mz
-      peak.mz <- xcms.features$mzmed
-      peak.id <-rownames(xcms.features)
-      adduct.matrix <- matrix(rep(adduct.mz,length(peak.mz)) , nrow = length(adduct.mz))
-      peak.matrix <- matrix(rep(peak.mz,length(adduct.mz)) ,
+      feature.mz <- xcms.features$mzmed
+      feature.id <-rownames(xcms.features)
+      adduct.matrix <- matrix(rep(adduct.mz,length(feature.mz)) , nrow = length(adduct.mz))
+      feature.matrix <- matrix(rep(feature.mz,length(adduct.mz)) ,
                             nrow = length(adduct.mz),
                             byrow = T)
-      sub.matrix <- adduct.matrix - peak.matrix
-      tol.matrix <- peak.matrix * ppm.thresh*1e-6
+      sub.matrix <- adduct.matrix - feature.matrix
+      tol.matrix <- feature.matrix * ppm.thresh*1e-6
       pass.matrix <- abs(sub.matrix) < tol.matrix
 
       matched.id <- which(pass.matrix,arr.ind = T)
       matched.id
 
       adduct <- data.frame( adduct.candidate[matched.id[,1],],
-                            peak.mz = xcms.features[matched.id[,2],"mzmed"],
-                            peak.id = peak.id[matched.id[,2]],
-                            peak.rt = xcms.features[matched.id[,2],"rtmed"],
-                            peak.intb = xcms.features.intb[matched.id[,2]])%>%
-        dplyr::mutate(peak.error = (peak.mz-exact.mz)/exact.mz*1e6, .before = peak.id)
-      rt_max <- adduct$peak.rt[which.max(adduct$peak.intb)]
+                            feature.mz = xcms.features[matched.id[,2],"mzmed"],
+                            feature.id = feature.id[matched.id[,2]],
+                            feature.rt = xcms.features[matched.id[,2],"rtmed"],
+                            feature.intb = xcms.features.intb[matched.id[,2]])%>%
+        dplyr::mutate(feature.error = (feature.mz-exact.mz)/exact.mz*1e6, .before = feature.id)
+      rt_max <- adduct$feature.rt[which.max(adduct$feature.intb)]
       adduct <- adduct%>%
-        dplyr::mutate(rt.filter = case_when(abs(peak.rt - rt_max) <rt.tol ~ T,
+        dplyr::mutate(rt.filter = case_when(abs(feature.rt - rt_max) <rt.tol ~ T,
                                             T ~F ))
 
       x[["adduct"]] <- adduct
