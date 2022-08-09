2f07ebf244338dc24b71363d4abb64af1849bb63
liRui <lirui3@shanghaitech.edu.cn>
2022-08-09 16:26:50 +0800

    craete function match_adduct_with_eicSimilarity extract feature's chromatogram and compare their similarity to main peak

---
+++ R/match_adduct_with_eicSimilarity.R
@@ -0,0 +1,37 @@
+#' @title match_adduct_with_eicSimilarity
+#' @description extract feature's chromatogram and compare their similarity to main peak
+#'
+#' @param MS.network
+#' @param xcms.xcms
+#'
+#' @return
+#' @export
+#'
+#' @examples
+match_adduct_with_eicSimilarity <-
+  function(MS.network, xcms.xcms) {
+    compare_eic <- function(MS.network , xcms.xcms) {
+      #x <- MS.network[[2]]
+      compound <- x[["compound"]]
+      adduct <- x[["adduct"]] %>%
+        dplyr::filter(rt.filter)
+
+      adduct
+      chrom <-
+        featureChromatograms(xcms.xcms , features = adduct$feature.id)
+      cor.matrix <- compareChromatograms(chrom)
+      cor.to.main.peak <- cor.matrix[which.max(adduct$feature.intb), ]
+      cor.to.main.peak[is.na(cor.to.main.peak)] <- 0
+      adduct$cor.to.main.peak <- cor.to.main.peak
+
+      x[["chromatogram"]] <- chrom
+      x[["adduct"]] <- adduct
+      return(x)
+
+    }
+
+    MS.network <- lapply(MS.network, compare_eic, xcms.xcms)
+    return(MS.network)
+
+
+  }
