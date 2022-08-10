e2b198bdd3901035b1cac3fc7211296ef34a2c43
liRui <lirui3@shanghaitech.edu.cn>
2022-08-10 15:12:24 +0800

    create function plot chromatogram

---
+++ R/plot_adduct_chromatogram.R
@@ -0,0 +1,43 @@
+plot_adduct_chromatogram <- function(MS.network,
+                                     index,
+                                     rt.filter = T,
+                                     cor.thresh = 0.7,
+                                     norm = F,move = T){
+
+  compound <- MS.network[[index]][["compound"]]
+  adduct.table <-MS.network[[index]][["adduct"]]
+  if (nrow(adduct.table)== 0 ) {
+    message("No adduct detected")
+    return()
+  }
+ #adduct.table <- dplyr::filter(adduct.table ,
+ #                              rt.filter,
+ #                              cor.to.main.peak >0)
+
+  idx.to.show <-which((adduct.table$rt.filter |!rt.filter )&adduct.table$cor.to.main.peak > cor.thresh)
+  chrom <-MS.network[[index]][["chromatogram"]]
+  if (isEmpty(idx.to.show)) {
+    message("No adduct after filter")
+    return()
+  }
+  adduct.table <- adduct.table[idx.to.show,]
+  if (length(idx.to.show)==1) {
+    chrom.col <- randomcoloR::distinctColorPalette(nrow(chrom))
+  }else{
+    chrom.col <- randomcoloR::distinctColorPalette(nrow(chrom))
+    chrom <- chrom[idx.to.show]
+  }
+
+
+
+
+  plot_XChromatograms(chrom,norm = norm,move = move)+
+    scale_color_manual(values = chrom.col,label = adduct.table$adduct)+
+    labs(title = paste0(compound["name"]),
+      x = "Retention time",y = "Intensity",col = "Adduct and isotope")
+
+
+
+
+}
+
