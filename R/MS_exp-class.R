acf48bab722ef5b21251e587f82a93c28f41edfa
liRui <lirui3@shanghaitech.edu.cn>
2022-08-11 16:02:21 +0800

    create class

---
+++ R/MS_exp-class.R
@@ -0,0 +1,10 @@
+setClass("MS_Exp",
+         slots =list(
+           "General" = "data.frame",
+           "Pre_process" = "data.frame",
+           "Moblie_phase_A" = "data.frame",
+           "Moblie_phase_b" = "data.frame",
+           "Chroma_gradient" = "data.frame",
+           "Mass_Spectrum" = "data.frame"
+         ))
+
