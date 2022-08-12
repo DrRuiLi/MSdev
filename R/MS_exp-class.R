b3e5b05cc40354a8223cc63004824499e7404a2c
LiRui <918791461@qq.com>
2022-08-12 09:46:09 +0800

    uncompleted

--- R/MS_exp-class.R
+++ R/MS_exp-class.R
@@ -1,10 +1,45 @@
 setClass("MS_Exp",
          slots =list(
-           "General" = "data.frame",
-           "Pre_process" = "data.frame",
-           "Moblie_phase_A" = "data.frame",
-           "Moblie_phase_b" = "data.frame",
-           "Chroma_gradient" = "data.frame",
-           "Mass_Spectrum" = "data.frame"
+           "General" = "tbl",
+           "Pre_process" = "tbl",
+           "Moblie_phase_A" = "tbl",
+           "Moblie_phase_b" = "tbl",
+           "Chroma_gradient" = "tbl",
+           "Mass_Spectrum" = "tbl"
+         ),
+         prototype = list(
+           General = tibble(
+             "MSC_id" =  "MSC0001",
+             "name" =  "Metabolomics",
+             "creat_time" = paste0(Sys.time()),
+             "instrument" = "SCIEX TripleTOF 6600",
+             "data_aquisition" = "DDA TOP10",
+             "link" = "",
+             "note" = ""
+           ),
+           Pre_process = tibble(
+
+           )
+
+
+
+
          ))
+
+MS_Exp <- function() {
+  new("MS_Exp")
+  }
+
+
+setMethod("show" ,"MS_Exp", definition = function(object){
+
+  general_data <- object@General
+  show(general_data)
+
+})
+
+
+object <- MS_Exp()
+
+
 
