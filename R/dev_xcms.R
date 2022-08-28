579a0031d204023d15c170d124e19f7255074f56
liRui <lirui3@shanghaitech.edu.cn>
2022-08-28 13:40:17 +0800

    move function to dev_xcms.R

--- R/dev_xcms.R
+++ R/dev_xcms.R
@@ -24,3 +24,115 @@ get_features_from_xcms <- function(xcms.xcms){
   SummarizedExperiment::rowData(xcms.sum) <-feature.def
   return(xcms.sum)
 }
+
+
+
+
+
+
+
+
+
+
+#' @title plot_xcms_peaks_distribution
+#' @description export peaks data by xcms::chromPeaks and plot by ggplot2
+#' @param xcms.xcms
+#' @param plot.title
+#'
+#' @return
+#' @export
+#'
+#' @examples
+plot_xcms_peaks_distribution <- function(xcms.xcms,plot.title = "Peaks distribution"){
+
+  xcms.peaks <- chromPeaks(xcms.xcms)%>%
+    as.data.frame()%>%
+    dplyr::filter(!is.na(intb))
+  xcms.process.type <- processHistory(xcms.xcms) %>% sapply( processType )
+  xcms.findpeak.param <- processHistory(xcms.xcms)[[which(xcms.process.type == "Peak detection")]]%>%
+    processParam()
+  ggplot(xcms.peaks)+
+    geom_point(aes(x = rt,y=mz,
+                   col = log10(intb),
+                   alpha = log10(intb)/10,
+                   size = (rtmax-rtmin)),
+    )+
+    scale_size_area(max_size = 8)+
+    labs(title = plot.title,
+         subtitle = paste0("ppm = ",xcms.findpeak.param@ppm,
+                           "; SN = ",xcms.findpeak.param@snthresh,
+                           "; prefilter = (",paste0(xcms.findpeak.param@prefilter,collapse = ","),")" ),
+         col = "Log10(Intensity)",
+         size = "Peak width",
+         x = "Retention time",
+         y = "mz")+
+    guides(alpha = "none")+
+    scale_color_gradientn(breaks = c(0,3,6,10),
+                          colors = c("white","orange","red","red"))+
+    theme(text = element_text(size = 8))->peaks.dis.plot
+  return(peaks.dis.plot)
+
+
+}
+
+
+
+#' @title plot_xcms_peaks_distribution
+#' @description plot_xcms_peaks_distribution
+#' @param xcms.xcms
+#' @param plot.title
+#'
+#' @return
+#' @export
+#'
+#' @examples
+plot_xcms_features_distribution <-
+  function(xcms.xcms, plot.title = "Features distribution") {
+    xcms.features <- featureDefinitions(xcms.xcms) %>%
+      as.data.frame() %>%
+      mutate(mz = mzmed, rt = rtmed)
+    xcms.features$intb <-
+      apply(featureValues(xcms.xcms, value = "intb"), 1, mean, na.rm = T)
+
+    xcms.process.type <-
+      processHistory(xcms.xcms) %>% sapply(processType)
+    xcms.findpeak.param <-
+      processHistory(xcms.xcms)[[which(xcms.process.type == "Peak detection")]] %>%
+      processParam()
+    ggplot(xcms.features) +
+      geom_point(aes(
+        x = rt,
+        y = mz,
+        col = log10(intb),
+        alpha = log10(intb) / 10,
+        size = (rtmax - rtmin)
+      ),) +
+      scale_size(range = c(2,6)) +
+      xlim(c(0, 800)) +
+      labs(
+        title = plot.title,
+        subtitle = paste0(
+          "ppm = ",
+          xcms.findpeak.param@ppm,
+          "; SN = ",
+          xcms.findpeak.param@snthresh,
+          "; prefilter = (",
+          paste0(xcms.findpeak.param@prefilter, collapse = ","),
+          ")"
+        ),
+        col = "Log10(Intensity)",
+        size = "Peak width",
+        x = "Retention time",
+        y = "mz"
+      ) +
+      guides(alpha = "none") +
+      scale_color_gradientn(breaks = c(0, 3, 6, 10),
+                            colors = c("white", "orange", "red", "red")) +
+      theme(text = element_text(size = 8)) -> peaks.dis.plot
+    return(peaks.dis.plot)
+
+
+
+
+
+  }
