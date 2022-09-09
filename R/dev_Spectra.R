57b5e8cd1a8ac353629728cd9a923f1060204d86
liRui <lirui3@shanghaitech.edu.cn>
2022-09-09 14:11:53 +0800

    create function

---
+++ R/dev_Spectra.R
@@ -0,0 +1,71 @@
+annotateSpectra <- function(expSpec,refSpec){
+
+  to.return <- list(
+    mz = median(precursorMz(expSpec)),
+    rt = median(rtime(expSpec)),
+    ref.mz = NA,
+    ref.rt = NA,
+    score = 0,
+    compound = NA,
+    adduct = NA,
+    inchikey = NA,
+    kegg.id = NA,
+    origin = NA,
+    "expSpec" = expSpec
+  )
+  if (length(refSpec)==0) {
+    return(to.return)
+  }
+
+  mz.error <- abs(matrixSub(expSpec$precursorMz , refSpec$precursorMz))/to.return$mz*1e6
+  mz.score <- 1 - mz.error/20 ###ppm 20
+
+  rt.error <- abs(matrixSub(expSpec$rtime , refSpec$rtime))
+  rt.score <- 2- rt.error/20
+  rt.score[is.na(rt.score)] <- 0
+
+  sp.score <- Spectra::compareSpectra(expSpec,refSpec)
+  sp.score[is.nan(sp.score)] <- 0
+
+  score <- mz.score*0.2 + rt.score*0.2 + sp.score*0.6
+
+
+    refSpecMatched <- refSpec[ceiling(which.max(score)/length(expSpec))]
+    score <- sp.score[which.max(score)]
+    to.return$ref.mz <- refSpecMatched$precursorMz
+    to.return$ref.rt <- refSpecMatched$rtime
+    to.return$score <- score
+    to.return$compound <- refSpecMatched$name
+    to.return$adduct <- refSpecMatched$adduct
+    to.return$inchikey <- refSpecMatched$inchikey
+    to.return$kegg.id <- refSpecMatched$kegg.id
+    to.return$origin <- refSpecMatched$database
+    to.return$refSpec <- refSpecMatched
+  return(to.return)
+
+
+
+
+}
+
+
+
+
+
+makeSpectra <- function(precursorMz ,
+                        rtime ,...){
+
+  Spectra::Spectra(DataFrame(precursorMz = precursorMz,
+                             rtime = rtime,
+                             ...))
+
+}
+normalizeSpectra <- function(z, ...) {
+  #z[,"maxIntensity"] <- max( z[, "intensity"] )
+  z[, "intensity"] <- z[, "intensity"] /
+    max(z[, "intensity"], na.rm = TRUE) * 100
+  z
+}
+
+
+
