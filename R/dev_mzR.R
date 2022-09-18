eefa595b9283f8b3eeb5215fbc3f564944a00356
liRui <lirui3@shanghaitech.edu.cn>
2022-09-18 15:27:01 +0800

    create function getmsExpTime

---
+++ R/dev_mzR.R
@@ -0,0 +1,20 @@
+#' @title getmsExpTime
+#' @description get MS experiment time by mzR::runInfo()$startTimeStamp
+#' @param msdataFiles
+#'
+#' @return a data.frame with two column: msDataFiles, ExpTime
+#' @export
+#'
+#' @examples
+getmsExpTime<- function(msDataFiles){
+  msdata <- data.frame(files = msDataFiles,
+                       ExpTime = NA)
+  for (i in 1:length(msDataFiles)) {
+    #cat(i,"\n")
+    msmzR <- mzR::openMSfile(msDataFiles[i])
+    msdata$ExpTime[i] <- mzR::runInfo(msmzR)$startTimeStamp
+    mzR::close(msmzR)
+  }
+  return(msdata)
+
+}
