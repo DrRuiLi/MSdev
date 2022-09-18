89c58ffde73ff4ab324cce19a15843e18151f2d7
liRui <lirui3@shanghaitech.edu.cn>
2022-09-18 15:26:37 +0800

    create fcuntion annotateSpectraMSdb copy from annotateSpectraMSdb, for future develop with MSdb

--- R/dev_Spectra.R
+++ R/dev_Spectra.R
@@ -48,7 +48,48 @@ annotateSpectra <- function(expSpec,refSpec){
 
 }
 
+annotateSpectraMSdb <- function(expSpec,refSpec){
 
+  to.return <- list(
+    mz = median(precursorMz(expSpec)),
+    rt = median(rtime(expSpec)),
+    ref.mz = NA,
+    ref.rt = NA,
+    score = 0,
+    MSDB_id =NA,
+    "expSpec" = expSpec,
+    "refSpec" = refSpec
+  )
+  if (length(refSpec)==0) {
+    return(to.return)
+  }
+
+  mz.error <- abs(matrixSub(expSpec$precursorMz , refSpec$precursorMz))/to.return$mz*1e6
+  mz.score <- 1 - mz.error/20 ###ppm 20
+
+  rt.error <- abs(matrixSub(expSpec$rtime , refSpec$rtime))
+  rt.score <- 2- rt.error/20
+  rt.score[is.na(rt.score)] <- 0
+
+  sp.score <- Spectra::compareSpectra(expSpec,refSpec)
+  sp.score[is.nan(sp.score)] <- 0
+
+  score <- mz.score*0.2 + rt.score*0.2 + sp.score*0.6
+
+
+  refSpecMatched <- refSpec[ceiling(which.max(score)/length(expSpec))]
+  score <- sp.score[which.max(score)]
+  to.return$ref.mz <- refSpecMatched$precursorMz
+  to.return$ref.rt <- refSpecMatched$rtime
+  to.return$score <- score
+  to.return$MSDB_id <-refSpecMatched$MSDB_id
+  to.return$refSpec <- refSpecMatched
+  return(to.return)
+
+
+
+
+}
 
 
 
