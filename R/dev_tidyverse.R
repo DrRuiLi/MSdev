91f615be9fa282e2d6b99521db36359af9e6dd59
liRui <lirui3@shanghaitech.edu.cn>
2022-10-27 16:23:17 +0800

    add function

--- R/dev_tidyverse.R
+++ R/dev_tidyverse.R
@@ -11,3 +11,41 @@ add_multi_column <- function(x, column_name){
     as.tibble()
 
 }
+
+
+
+
+
+#' @title list2df
+#'
+#' @description convert list to data.frame. In each list,
+#' sub-list will be replaced with "large list" and missing value will be fill by NA.
+#' See `data.table::rbindlist`
+#'
+#' @param x
+#'
+#' @return
+#' @export
+#'
+#' @examples
+list2df <- function(x){
+
+  list.vars <- sapply(x, names)%>%
+    unlist()%>%
+    unique()
+
+  lapply(x , function(z){
+    z <- z[list.vars]
+    z.is.list <- sapply(z, is.list)
+    z[z.is.list] <- "large list"
+    z.is.null <- sapply(z , is.null)
+    z[z.is.null] <- NA
+    z
+  })%>%
+    data.table::rbindlist(fill = T)%>%
+    as.data.frame()->x.df
+  ### empty column with name NA will lead to error, unknown reason
+  x.df <- x.df[,which(!is.na(colnames(x.df)))]
+  x.df
+
+}
