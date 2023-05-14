0247e1f538e2bee952f09d3819765978fc25b3c7
liRui <lirui3@shanghaitech.edu.cn>
2023-05-14 16:20:07 +0800

    move this function to dev.base

--- R/dev_base.R
+++ R/dev_base.R
@@ -217,3 +217,29 @@ median_part <- function(x, n = 10){
   x.median
 }
 
+
+
+
+#' @title normalize_max_min
+#'
+#' @param x
+#'
+#' @return
+#' @export
+#'
+#' @examples
+normalize_max_min<- function(x){
+
+  f <- function(z){
+    (z-min(z,na.rm = T))/(max(z,na.rm = T)-min(z,na.rm = T))
+  }
+  if (is.vector(x)) {
+    return(f(x))
+  }
+  if (is.matrix(x)|is.data.frame(x)) {
+    apply(x, 1, f)%>%t%>%return()
+  }
+
+}
+
+
