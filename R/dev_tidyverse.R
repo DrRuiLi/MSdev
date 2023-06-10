4590a59ba4c0b4f5b7863c1b90c83cdca27c7faf
liRui <lirui3@shanghaitech.edu.cn>
2023-06-10 11:54:08 +0800

    add function

--- R/dev_tidyverse.R
+++ R/dev_tidyverse.R
@@ -8,7 +8,7 @@ add_multi_column <- function(x, column_name){
                       ncol = length(column_name))
   colnames(df.to.add) <- column_name
   cbind(x , df.to.add)%>%
-    as.tibble()
+    as_tibble()
 
 }
 
@@ -47,5 +47,22 @@ list2df <- function(x){
   ### empty column with name NA will lead to error, unknown reason
   x.df <- x.df[,which(!is.na(colnames(x.df)))]
   x.df
+
+}
+
+
+
+
+
+
+
+dplyr_copy_row <- function(df,nrow.idx,n){
+
+  df.to.add <- df[nrow.idx,] %>%
+    add_multi_column(1:n)%>%
+    tidyr::pivot_longer(as.character(1:n))%>%
+    dplyr::select(-name,-value)
+  rbind(df,df.to.add)
+
 
 }
