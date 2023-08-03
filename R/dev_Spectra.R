515b92b1766d08bf2cd69add6855e04af61a1cf6
LiRui <lirui3@shangahitech.edu.cn>
2023-08-03 15:43:14 +0800

    add some function to manusplit Spectra

--- R/dev_Spectra.R
+++ R/dev_Spectra.R
@@ -105,10 +105,25 @@ makeSpectra <- function(precursorMz ,
 }
 
 
-filterSpectraIntensity <- function(sp,r){
+filterSpectraIntensity <- function(sp,ratio){
 
-  Spectra::filterIntensity( sp, intensity =c(max(intensity(sp))*r,Inf   ) )
+  Spectra::filterIntensity( sp, intensity = function(z) z/max(z) > r )
 
+}
+
+
+filterSpectra_below_PrecursorMz <- function(sp){
+
+  if (!length(sp)) return(sp)
+  nf <-  function(z,  precursorMz ,...) {
+
+    idx <- z[, "mz"] < precursorMz-0.5
+    z[idx,,drop = F]
+  }
+  sp <- Spectra::addProcessing(sp,FUN = nf, spectraVariables = "precursorMz")%>%
+    applyProcessing(BPPARAM = SerialParam())
+
+  return(sp)
 }
 
 
@@ -126,3 +141,136 @@ normalizeSpectra <- function(sp){
   return(sp)
 
 }
+
+normalizeSpectra_by_precursorIntensity <- function(sp){
+
+  if (!length(sp)) return(sp)
+  sp$precursorIntensity[sp$precursorIntensity==0] <- sp$totIonCurrent[sp$precursorIntensity==0]
+  nf <-  function(z,  precursorIntensity ,...) {
+    #z[,"maxIntensity"] <- max( z[, "intensity"] )
+    z[, "intensity"] <- z[, "intensity"] / precursorIntensity * 100
+    z
+  }
+  sp <- Spectra::addProcessing(sp,FUN = nf, spectraVariables = "precursorIntensity")%>%
+    applyProcessing(BPPARAM = SerialParam())
+
+  return(sp)
+
+}
+
+#' get_Spectra_data
+#'
+#' @param sp `Spectra` object
+#' @param var any variables of `Spectra::spectraVariables()`
+#'
+#' @return a dataframe with mz and intensity and vars
+#' @export
+#'
+#' @examples
+get_Spectra_data <- function(sp,var = c("precursorMz","collisionEnergy")){
+
+
+  if (!length(sp)) {
+    sp.data <- data.frame(matrix(nrow = 0,ncol = 3+length(var)))%>%
+      `colnames<-`(c("sp.id","mz","intensity",var))
+      return(sp.data)
+  }
+  sp.data <- Spectra::spectraData(sp)%>%
+    as.data.frame()%>%
+    dplyr::mutate(id = 1:n())
+
+  mz <- mz(sp)%>%
+    as.data.frame()%>%
+    dplyr::select(everything(),mz = value)
+  int <- intensity(sp)%>%
+    as.data.frame()%>%
+    dplyr::select(everything(),intensity = value)
+
+  spec.df <- mz %>%
+    dplyr::mutate(int,
+                  sp.id = group,
+                  sp.data[match(sp.id,sp.data$id)   ,var])%>%
+    dplyr::select(sp.id ,!all_of(c("group","group_name")))
+  return(spec.df)
+}
+
+combineSpectra_groupby_ce <- function(sp,minProp = 0.5){
+
+  combineSpectra(sp,peaks = "intersect",
+                 f = sp$collisionEnergy,
+                 minProp=minProp,
+                 ppm = 25)
+
+
+}
+
+
+
+get_Spectra_transition <- function(sp){
+
+  sp <- filterSpectra_below_PrecursorMz(sp)
+  sp.data <- get_Spectra_data(sp)%>%
+    dplyr::mutate(groupMz(mz))%>%
+    dplyr::group_by(mz.group )%>%
+    dplyr::mutate(mz.group.max = max(intensity),
+                  mz.group.n = n())%>%
+    dplyr::slice_max(intensity , n = 1)%>%
+    dplyr::ungroup()%>%
+    dplyr::slice_max(intensity , n = 2)%>%
+    dplyr::select( precursorMz , productMz =  mz,
+                   collisionEnergy , productRatio = intensity)
+
+  return(sp.data)
+
+}
+
+
+ggplotSpectraMirror<- function(sp1,sp2){
+
+  sp.data <-rbind(get_Spectra_data(sp1)%>%
+                    dplyr::mutate(sp.id=1),
+                  get_Spectra_data(sp2)%>%
+                    dplyr::mutate(sp.id=2))%>%
+    dplyr::mutate(groupMz(mz))%>%
+    dplyr::mutate(x = mz,
+                  xend = mz,
+                  y = 0,
+                  yend = case_when(sp.id==1~intensity,
+                                   T~-intensity))%>%
+    dplyr::group_by(mz.group)%>%
+    dplyr::mutate(matched = n()==2)
+
+  label.df <- dplyr::filter(sp.data , matched)
+  ymax.abs <- max(abs(sp.data$intensity))
+  ggplot(sp.data)+
+    geom_hline(yintercept = 0 , size = 0.2,col = "grey")+
+    geom_segment(aes(x = x,y =y,xend = xend,
+                     yend = yend,col = matched),
+                 size = 0.2,
+                 show.legend = F)+
+    geom_point(aes(x = x, y = yend , alpha = matched,col = matched),
+               show.legend = F,size = 0.5)+
+    ggrepel::geom_text_repel(aes(x = x, y = yend ,
+                                 label = format(mz,digit = 4,nsmall = 4)),
+                             size =2,
+                             col = "#00000088",
+                             segment.size = 0.1,
+                             data = label.df)+
+    scale_color_manual(values = c(`FALSE` = "grey",`TRUE` = "#80B1D3"))+
+    scale_alpha_manual(values = c(`FALSE` = 0,`TRUE` =1))+
+    ylim(c(-ymax.abs,ymax.abs))+
+    labs(x = "Mz",y = "Intensity")+
+    theme_classic()+
+    theme(axis.line = element_line(linewidth = 0.1),
+          axis.ticks = element_line(linewidth = 0.1))->p
+  p
+
+
+}
+
+
+
+
+
+
+
