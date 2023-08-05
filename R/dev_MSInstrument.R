49bd57efc94ee791915d28e34f3f439e3dc1602f
LiRui <lirui3@shangahitech.edu.cn>
2023-08-05 14:11:57 +0800

    update function to expand retention time

--- R/dev_MSInstrument.R
+++ R/dev_MSInstrument.R
@@ -1,7 +1,7 @@
 export_QE_InclusionList_From_xcmsFeature <- function(xcms.xcms){
 
   feature.def <- featureDefinitions_PeakSta(xcms.xcms)
-  ion_mode <- unique(fData(xcms.xcms)$polarity)
+  ion_mode <- unique(Biobase::fData(xcms.xcms)$polarity)
   if (length(ion_mode)>1) {
     stop("multiple polarity")
 
@@ -12,11 +12,12 @@ export_QE_InclusionList_From_xcmsFeature <- function(xcms.xcms){
   inclusion.list <- QEinclusionListTemplate[rep(1,nrow(feature.def)),]
   inclusion.list$`Mass [m/z]` <- feature.def$mzmed
   inclusion.list$Polarity <- ion_mode
-  inclusion.list$`Start [min]` <- feature.def$peakRtMin/60
-  inclusion.list$`End [min]` <- feature.def$peakRtMax/60
-  inclusion.list$Comment <- "Export from xcms"
-  inclusion.list$maxo <-feature.def$peakMaxo%>%log10
-  inclusion.list
+  inclusion.list$`Start [min]` <- (feature.def$peakRtMin-10)%>%
+    ifelse(. < 0 ,0,.)/60
+  inclusion.list$`End [min]` <- (feature.def$peakRtMax+10) %>%
+    ifelse(. >max(rtime(xcms.xcms)),max(rtime(xcms.xcms)),.)/60
+  inclusion.list$Comment <- "Export from MSdev"
+  return(inclusion.list)
 }
 
 export_QE_ExclusionList_From_xcmsPeaks <- function(xcms.xcms,peak.count.thresh = 10){
