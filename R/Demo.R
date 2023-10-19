ae324828c7000cec2ff3663afa85320784e66c70
LiRui <lirui3@shangahitech.edu.cn>
2023-10-19 18:52:34 +0800

    create demo

---
+++ R/Demo.R
@@ -0,0 +1,11 @@
+load_demo <- function(demo = c("MSdev","XCMSnExp")){
+
+  demo <-match.arg(demo)
+  file_path <- switch(demo,
+         "MSdev" = "C:\\Users\\91879\\OneDrive\\Documents\\Code\\R\\Projecct\\2022.1.8_MS.demo\\Demo/MSdev_2023_10_19.Rdata",
+         "XCMSnExp" = "C:\\Users\\91879\\OneDrive\\Documents\\Code\\R\\Projecct\\2022.1.8_MS.demo\\Demo/XCMSnExp_2023_10_19.rda")
+
+  demo <- load_as_var(file_path)
+
+  return(demo)
+}
