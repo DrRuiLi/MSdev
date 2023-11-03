8fdbbab0af0f2ec82d3cdf542de7e6989a76cbc6
LiRui <lirui3@shangahitech.edu.cn>
2023-11-03 16:23:48 +0800

    add data.se demo

--- R/Demo.R
+++ R/Demo.R
@@ -1,11 +1,13 @@
-load_demo <- function(demo = c("MSdev","XCMSnExp","xcms")){
+load_demo <- function(demo = c("MSdev","XCMSnExp","xcms","SummarizedExperiment","data.se")){
 
   demo <-match.arg(demo)
   file_path <- switch(demo,
          "MSdev" = "C:\\Users\\91879\\OneDrive\\Code\\R\\Projecct\\2022.1.8_MS.demo\\Demo/MSdev_2023_10_19.Rdata",
          "XCMSnExp" = "C:\\Users\\91879\\OneDrive\\Code\\R\\Projecct\\2022.1.8_MS.demo\\Demo/XCMSnExp_2023_10_19.rda",
-         "xcms" = "C:\\Users\\91879\\OneDrive\\Code\\R\\Projecct\\2022.1.8_MS.demo\\Demo/XCMSnExp_2023_10_19.rda")
-
+         "xcms" = "C:\\Users\\91879\\OneDrive\\Code\\R\\Projecct\\2022.1.8_MS.demo\\Demo/XCMSnExp_2023_10_19.rda",
+          "SummarizedExperiment" = "C:\\Users\\91879\\OneDrive\\Code\\R\\Projecct\\2022.1.8_MS.demo\\Demo/SummarizedExperiment_2023_11_03.rda",
+         "data.se" = "C:\\Users\\91879\\OneDrive\\Code\\R\\Projecct\\2022.1.8_MS.demo\\Demo/SummarizedExperiment_2023_11_03.rda"
+         )
   demo <- load_as_var(file_path)
 
   return(demo)
