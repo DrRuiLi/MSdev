6601bd680ef0645d05f78283c11cf1769c7c617a
LiRui <918791461@qq.com>
2025-12-02 15:54:09 +0800

    open file and set dir

--- R/dev_base.R
+++ R/dev_base.R
@@ -144,7 +144,7 @@ open_MSIP <- function(){
 open_PAVE <- function(){
   pave.file <- "C:/Users/91879/OneDrive/Documents/YLF_Lab/Project/2025.10.10.PAVE/code/PAVE_data_Analysis.R"
   rstudioapi::documentOpen(pave.file)
-
+  setRStudioDir()
   return(pave.file)
 }
 
