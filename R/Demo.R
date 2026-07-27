913729a6060211cb70650b91cc5b3f2fe3abd9ba
LiRui <918791461@qq.com>
2026-07-27 14:50:29 +0800

    refactor: Update demo loading function and documentation
    - Revised the `load_demo` function to include `XcmsExperiment` as a preferred alias for `xcms`, enhancing clarity in the parameter options.
    - Updated file path handling to use `file.path()` for better cross-platform compatibility.
    - Improved documentation to reflect the changes in parameter names and provide clearer guidance on available demo types.

--- R/Demo.R
+++ R/Demo.R
@@ -1,9 +1,11 @@
 #' @title Load bundled demo data objects
 #' @description Load pre-built demo objects from the MSdev demo project directory.
 #' Dispatches to [MSdev_load()] for `"MSdev"` and `readRDS()` for other demo types.
+#' `"xcms"` is an alias for `"XcmsExperiment"` (preferred). Use `"XCMSnExp"` for the
+#' legacy on-disk `XCMSnExp` demo.
 #'
-#' @param demo Character. One of `"MSdev"`, `"XCMSnExp"`, `"xcms"`,
-#'   `"SummarizedExperiment"`, `"data.se"`, `"Spectra"`, or `"sp"`.
+#' @param demo Character. One of `"MSdev"`, `"XcmsExperiment"`, `"xcms"`,
+#'   `"XCMSnExp"`, `"SummarizedExperiment"`, `"data.se"`, `"Spectra"`, or `"sp"`.
 #'
 #' @return The loaded demo object (type depends on `demo`).
 #'
@@ -11,37 +13,42 @@
 #' \dontrun{
 #' msdev.demo <- load_demo("MSdev")
 #' xcms.demo <- load_demo("xcms")
+#' xe.demo <- load_demo("XcmsExperiment")
 #' }
 #'
 #' @export
 load_demo <- function(demo = c("MSdev",
-                               "XCMSnExp","xcms",
-                               "SummarizedExperiment","data.se",
-                               "Spectra","sp")){
+                               "XcmsExperiment", "xcms",
+                               "XCMSnExp",
+                               "SummarizedExperiment", "data.se",
+                               "Spectra", "sp")) {
 
-  demo <-match.arg(demo)
-  file_path <- switch(demo,
-         "MSdev" = "C:\\Users\\91879\\OneDrive\\Code\\R\\Projecct\\2022.1.8_MS.demo\\Demo/MSdev_2025_04_01.Rdata",
-         "XCMSnExp" = "C:\\Users\\91879\\OneDrive\\Code\\R\\Projecct\\2022.1.8_MS.demo\\Demo/XCMSnExp_2023_11_17.rda",
-         "xcms" = "C:\\Users\\91879\\OneDrive\\Code\\R\\Projecct\\2022.1.8_MS.demo\\Demo/XCMSnExp_2023_11_17.rda",
-         "SummarizedExperiment" = "C:\\Users\\91879\\OneDrive\\Code\\R\\Projecct\\2022.1.8_MS.demo\\Demo/SummarizedExperiment_2023_11_03.rda",
-         "data.se" = "C:\\Users\\91879\\OneDrive\\Code\\R\\Projecct\\2022.1.8_MS.demo\\Demo/SummarizedExperiment_2023_11_03.rda",
-         "Spectra" = "C:\\Users\\91879\\OneDrive\\Code\\R\\Projecct\\2022.1.8_MS.demo\\Demo/Spectra_2023_11_23.rda",
-         "sp" = "C:\\Users\\91879\\OneDrive\\Code\\R\\Projecct\\2022.1.8_MS.demo\\Demo/Spectra_2023_11_23.rda"
-         )
-
-  fun <- switch(demo,
-                "MSdev" = MSdev_load,
-                "XCMSnExp" = readRDS,
-                "xcms" = readRDS,
-                "SummarizedExperiment" = readRDS,
-                "data.se" = readRDS,
-                "Spectra" = readRDS,
-                "sp" = readRDS
+  demo <- match.arg(demo)
+  demo_dir <- "C:\\Users\\91879\\OneDrive\\Code\\R\\Projecct\\2022.1.8_MS.demo\\Demo"
+  file_path <- switch(
+    demo,
+    "MSdev" = file.path(demo_dir, "MSdev_2025_04_01.Rdata"),
+    "XcmsExperiment" = file.path(demo_dir, "XcmsExperiment_2023_11_17.rda"),
+    "xcms" = file.path(demo_dir, "XcmsExperiment_2023_11_17.rda"),
+    "XCMSnExp" = file.path(demo_dir, "XCMSnExp_2023_11_17.rda"),
+    "SummarizedExperiment" = file.path(demo_dir, "SummarizedExperiment_2023_11_03.rda"),
+    "data.se" = file.path(demo_dir, "SummarizedExperiment_2023_11_03.rda"),
+    "Spectra" = file.path(demo_dir, "Spectra_2023_11_23.rda"),
+    "sp" = file.path(demo_dir, "Spectra_2023_11_23.rda")
   )
-  demo <- fun(file_path)
 
-  return(demo)
+  fun <- switch(
+    demo,
+    "MSdev" = MSdev_load,
+    "XcmsExperiment" = readRDS,
+    "xcms" = readRDS,
+    "XCMSnExp" = readRDS,
+    "SummarizedExperiment" = readRDS,
+    "data.se" = readRDS,
+    "Spectra" = readRDS,
+    "sp" = readRDS
+  )
+  fun(file_path)
 }
 
 make_demo <- function(){
